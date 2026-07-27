6f27c870ec296f2a277427b869127a5391ad184e
omaralirayes-del <omaralirayes@gmail.com>
2026-07-27 20:19:33 +0300

    Fix root build.gradle.kts script and clean dependencies

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+buildscript {
+    extra["kotlin_version"] = "2.0.21"
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
+    }
+}
+
 allprojects {
     repositories {
         google()
